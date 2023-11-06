7024b8e2e5acfe35fe4b2e109ba14dc78a278eb9
kristof01124 <kristof01124@mailbox.unideb.hu>
2023-11-06 20:45:32 +0100

    Refactoring

--- src/main/java/hu/unideb/inf/webfejlesztesbeadando/repository/OrderRepository.java
+++ src/main/java/hu/unideb/inf/webfejlesztesbeadando/repository/OrderRepository.java
@@ -1,9 +1,9 @@
 package hu.unideb.inf.webfejlesztesbeadando.repository;
 
-import hu.unideb.inf.webfejlesztesbeadando.entity.OrderDTO;
+import hu.unideb.inf.webfejlesztesbeadando.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<OrderDTO, Long> {
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }
--- src/main/java/hu/unideb/inf/webfejlesztesbeadando/repository/SiteUserRepository.java
+++ src/main/java/hu/unideb/inf/webfejlesztesbeadando/repository/SiteUserRepository.java
@@ -1,9 +1,9 @@
 package hu.unideb.inf.webfejlesztesbeadando.repository;
 
-import hu.unideb.inf.webfejlesztesbeadando.entity.SiteUserDTO;
+import hu.unideb.inf.webfejlesztesbeadando.entity.SiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SiteUserRepository  extends JpaRepository<SiteUserDTO, Long> {
+public interface SiteUserRepository  extends JpaRepository<SiteUser, Long> {
 }
