6cf21e095c8801d1c8fa42c9ee493eda9bc30416
kristof01124 <kristof01124@mailbox.unideb.hu>
2023-11-06 20:45:18 +0100

    Created DTOs

---
+++ src/main/java/hu/unideb/inf/webfejlesztesbeadando/DTO/OrderDTO.java
@@ -0,0 +1,3 @@
+package hu.unideb.inf.webfejlesztesbeadando.DTO;
+
+public record OrderDTO(Long orderId, String orderName, double price, Long siteUserId) {}
---
+++ src/main/java/hu/unideb/inf/webfejlesztesbeadando/DTO/SiteUserDTO.java
@@ -0,0 +1,3 @@
+package hu.unideb.inf.webfejlesztesbeadando.DTO;
+
+public record SiteUserDTO(Long userId, String firstName, String lastName, String username, String password) {}
