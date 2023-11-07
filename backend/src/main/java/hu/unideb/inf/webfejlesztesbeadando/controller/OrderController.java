153719ac82d2828ff37896805a39322bb40cc46d
kristof01124 <kristof01124@mailbox.unideb.hu>
2023-11-07 09:37:48 +0100

    Restructureing folders

--- backend/src/main/java/hu/unideb/inf/webfejlesztesbeadando/controller/OrderController.java
+++ backend/src/main/java/hu/unideb/inf/webfejlesztesbeadando/controller/OrderController.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController()
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/orders")
 public class OrderController {
 
--- backend/src/main/java/hu/unideb/inf/webfejlesztesbeadando/controller/SiteUserController.java
+++ backend/src/main/java/hu/unideb/inf/webfejlesztesbeadando/controller/SiteUserController.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController()
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/siteUsers")
 public class SiteUserController {
 
